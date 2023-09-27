184dd18b8575a511edc45dc660dc440b5e795abb
josueBarretogit <josuebarretogit@gmail.com>
2023-09-27 09:51:43 -0500

    implementacion loading screen y consumo api exitosa

--- lib/model/Anillo.dart
+++ lib/model/Anillo.dart
@@ -1,23 +1,29 @@
 class Anillo {
+  final int id;
   final String nombre;
-  final String peso;
+  final Map<String, dynamic> foto;
+  final List<dynamic>? pesos;
   final String talla;
   final String referencia;
   final String categoria;
 
   Anillo(
       {required this.nombre,
-      required this.peso,
+      required this.id,
+      required this.foto,
+      required this.pesos,
       required this.talla,
       required this.referencia,
       required this.categoria});
 
   factory Anillo.fromJson(Map<String, dynamic> json) {
     return Anillo(
+      id: json['id'],
+      foto: json['foto'],
       nombre: json['nombre'],
-      peso: json['peso'],
+      pesos: json['pesos'],
       talla: json['talla'],
-      referencia: json['refrencia'],
+      referencia: json['referencia'],
       categoria: json['categoria'],
     );
   }
--- lib/widgets/Mobile/mobileWidgets/datosAnillo.dart
+++ lib/widgets/Mobile/mobileWidgets/datosAnillo.dart
@@ -3,11 +3,15 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:anillos_jalbac_flutter/model/Anillo.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-Future<Anillo> getAnillo(int? id) async {
+Future<Anillo> getAnillo(int id) async {
   final response = await http.post(
-    Uri.parse('localhost:4000/api/anillos/getone'),
-    body: jsonEncode(<String, int?>{
+    Uri.parse('http://10.0.2.2:4000/api/anillos/getone'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, int>{
       'id': id,
     }),
   );
@@ -15,6 +19,7 @@ Future<Anillo> getAnillo(int? id) async {
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
+    print(response.body);
     return Anillo.fromJson(jsonDecode(response.body));
   } else {
     // If the server did not return a 200 OK response,
@@ -45,39 +50,47 @@ class _DatosAnilloState extends State<DatosAnillo> {
     return FutureBuilder<Anillo>(
       future: futureAnillo,
       builder: (context, snapshot) {
-        return Column(
-          children: [
-            Row(
-              children: [
-                Text("Nombre : ", style: textStyle),
-                Text(snapshot.data!.nombre, style: textStyle),
-              ],
-            ),
-            Row(
-              children: [
-                Text("Peso : ", style: textStyle),
-                Text(snapshot.data!.peso, style: textStyle),
-              ],
-            ),
-            Row(
-              children: [
-                Text("Talla: ", style: textStyle),
-                Text(snapshot.data!.talla, style: textStyle),
-              ],
-            ),
-            Row(
-              children: [
-                Text("Referencia : ", style: textStyle),
-                Text(snapshot.data!.referencia, style: textStyle),
-              ],
-            ),
-            Row(
-              children: [
-                Text("Categoria: ", style: textStyle),
-                Text(snapshot.data!.categoria, style: textStyle),
-              ],
-            )
-          ],
+        if (snapshot.hasData) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  Text("Nombre : ", style: textStyle),
+                  Text(snapshot.data!.nombre, style: textStyle),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Talla: ", style: textStyle),
+                  Text(snapshot.data!.talla, style: textStyle),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Referencia : ", style: textStyle),
+                  Text(snapshot.data!.referencia, style: textStyle),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Categoria: ", style: textStyle),
+                  Text(snapshot.data!.categoria, style: textStyle),
+                ],
+              )
+            ],
+          );
+        } else if (snapshot.hasError) {
+          print(snapshot.error);
+
+          return Text('there was an error ${snapshot.error}');
+        }
+        return Center(
+          child: LoadingAnimationWidget.discreteCircle(
+            color: Colors.white,
+            size: 100,
+          ),
         );
       },
     );
